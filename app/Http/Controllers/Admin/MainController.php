0407819b62d0190d7c6ac1b4cb8d686d1f234147
“dangloc” <dangloc522@gmail.com>
2022-06-30 23:25:09 +0700

    login_continues

---
+++ app/Http/Controllers/Admin/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index(){
+        echo 'Admin Dang Ba Loc da dang nhap thanh cong';
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // public function setPasswordAttribute($value)
+    // {
+    //     if( \Hash::needsRehash($value) ) {
+    //         $value = \Hash::make($value);
+    //     }
+    //     $this->attributes['password'] = $value;
+    // }
+
 }
