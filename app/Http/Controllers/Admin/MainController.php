62078636d55ec686755ad275a0bf044e3260418c
“dangloc” <dangloc522@gmail.com>
2022-07-01 15:14:29 +0700

    display_admin done

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     public function index(){
-        echo 'Admin Dang Ba Loc da dang nhap thanh cong';
+       return view('admin.home', ['title'=> 'Admintrator page']);
     }
 }
