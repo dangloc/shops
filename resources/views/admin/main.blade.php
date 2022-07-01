e6324e47504f332ac8a324560f7f32221b2fcff8
“dangloc” <dangloc522@gmail.com>
2022-07-01 22:02:09 +0700

    done add & list category menu_admin

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -7,15 +7,12 @@
 <div class="wrapper">
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       
     </ul>
-
-    <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto"> 
       <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
@@ -32,6 +29,7 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
+        @include('admin.alert')
         <div class="row">
           <!-- left column -->
           <div class="col-md-12">
