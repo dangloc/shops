e4a3d8722dcb8c6ff2c4195f4976982a4d5354f1
“dangloc” <dangloc522@gmail.com>
2022-07-12 00:25:51 +0700

    done load slider

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,17 +10,19 @@ use App\Http\Services\product\ProductService;
 class MainController extends Controller
 {
     protected $menu;
+    protected $slider;
 
-    public function __construct(MenuService $menu)
+    public function __construct(MenuService $menu, SliderService $slider)
     {
         $this->menu = $menu;
+        $this->slider = $slider;
     }
 
     public function index()
     {
         return view('main', [
             'title' => 'Shoe shop',
-            // 'sliders' => $this->slider->show(),
+            'sliders' => $this->slider->show(),
             'menus' => $this->menu->show(),
             // 'products' => $this->product->get()
         ]);
